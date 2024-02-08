18478aeeb8e6f978674619fd72e8d5ec1aa69414
Makarand Hinge <mmh2212002@gmail.com>
2024-02-08 23:00:48 +0530

    Digital Clock using vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <DigitalClock />
+  </div>
+</template>
+
+<script>
+import DigitalClock from './DigitalClock.vue';
+
+export default {
+  components: {
+    DigitalClock
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+}
+</style>
---
+++ src/DigitalClock.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="digital-clock">
+    {{ currentTime }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentTime: ""
+    };
+  },
+  mounted() {
+    this.updateTime();
+    setInterval(() => {
+      this.updateTime();
+    }, 1000);
+  },
+  methods: {
+    updateTime() {
+      this.currentTime = new Date().toLocaleTimeString();
+    }
+  }
+};
+</script>
+
+<style>
+.digital-clock {
+  font-size: 24px;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
